4aad7d1efac639bc12d9da17bf08aba9c2b2a500
Adrian Wieprzkowicz <Argeento@users.noreply.github.com>
2024-11-04 18:46:33 +0100

    Fix config copy to frpc

--- src/run.sh
+++ src/run.sh
@@ -59,29 +59,23 @@ SERVER_ADDRESS=$(jq --raw-output '.server' /data/options.json)
 AUTH_TOKEN=$(jq --raw-output '.token' /data/options.json)
 SUBDOMAIN=$(jq --raw-output '.subdomain' /data/options.json)
 PROXY_NAME=$(jq --raw-output '.proxy_name' /data/options.json)
-LOCAL_PORT=$(jq --raw-output '.local_port' /data/options.json)
-USE_ENCRYPTION=$(jq --raw-output '.use_encryption' /data/options.json)
-USE_COMPRESSION=$(jq --raw-output '.use_compression' /data/options.json)
+LOCAL_PORT=$(jq --raw-output '.port' /data/options.json)
+USE_ENCRYPTION=$(jq --raw-output '.useEncryption' /data/options.json)
+USE_COMPRESSION=$(jq --raw-output '.useCompression' /data/options.json)
 PROTOCOL=$(jq --raw-output '.protocol' /data/options.json)
 
-# Ścieżka do pliku konfiguracyjnego FRP
+# Tworzenie pliku konfiguracyjnego frpc.toml bezpośrednio
 FRP_CONFIG_PATH="./frpc.toml"
-
-# Sprawdzenie istnienia pliku konfiguracyjnego FRP
-if [ ! -f "$FRP_CONFIG_PATH" ]; then
-    echo "Plik konfiguracyjny FRP nie istnieje: $FRP_CONFIG_PATH"
-    exit 1
-fi
-
-# Aktualizacja konfiguracji FRP na podstawie danych z Home Assistant
-sed -i "s|__arg_server_address__|$SERVER_ADDRESS|g" $FRP_CONFIG_PATH
-sed -i "s|__arg_auth_token__|$AUTH_TOKEN|g" $FRP_CONFIG_PATH
-sed -i "s|__arg_subdomain__|$SUBDOMAIN|g" $FRP_CONFIG_PATH
-sed -i "s|__arg_proxy_name__|$PROXY_NAME|g" $FRP_CONFIG_PATH
-sed -i "s|__arg_local_port__|$LOCAL_PORT|g" $FRP_CONFIG_PATH
-sed -i "s|__arg_use_encryption__|$USE_ENCRYPTION|g" $FRP_CONFIG_PATH
-sed -i "s|__arg_use_compression__|$USE_COMPRESSION|g" $FRP_CONFIG_PATH
-sed -i "s|__arg_protocol__|$PROTOCOL|g" $FRP_CONFIG_PATH
+cat <<EOF > $FRP_CONFIG_PATH
+[[proxies]]
+name = "$PROXY_NAME"
+type = "$PROTOCOL"
+localIP = "127.0.0.1"
+localPort = $LOCAL_PORT
+subdomain = "$SUBDOMAIN"
+transport.useEncryption = $USE_ENCRYPTION
+transport.useCompression = $USE_COMPRESSION
+EOF
 
 # Wyświetlenie zawartości pliku frpc.toml po dokonaniu zmian
 echo "Zawartość pliku frpc.toml po zmianach:"
